7d8605b52b7d87cf30a1e2a93c4c2bfb155ae37d
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-04 18:30:03 +0530

    created navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-5">
-      <h1>Portfolio</h1>
-      <h3>Hey Atharva here, glad that you find me</h3>
     </main>
   )
 }
