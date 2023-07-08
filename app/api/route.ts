6d979219f1ed902a1504209eef61ac0623108f4c
Atharva Jadhav <jadhavatharva499@gmail.com>
2023-07-08 11:41:09 +0530

    route working - dont use default export

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,24 +1,31 @@
-// export default function handler(
-//   req: {
-//     body: { email: string; name: string; message: string };
-//     method: string;
-//   },
-//   res: {
-//     status: (arg0: number) => {
-//       (): any;
-//       new (): any;
-//       json: { (arg0: { name: string }): void; new (): any };
-//     };
-//   }
-// ) {
-//     console.log('fuckkkkkkkkkkk')
-//   if (req.method === "post") {
-//     alert("Done request");
-//   }
-//   res.status(200).json({ name: "John Doe" });
-// }
+export default function handler(
+  req: {
+    body: { email: string; name: string; message: string };
+    method: string;
+  },
+  res: {
+    status: (arg0: number) => {
+      (): any;
+      new (): any;
+      json: { (arg0: { name: string }): void; new (): any };
+    };
+  }
+) {
+    console.log('fuckkkkkkkkkkk')
+  if (req.method === "post") {
+    alert("Done request");
+  }
+  res.status(200).json({ name: "John Doe" });
+}
 
-export default function POST(request: Request) {
+import { NextResponse } from "next/server";
+
+export function POST(request: Request) {
       console.log('fuckkkkkkkkkkk')
   console.log(request.body);
+  return NextResponse.json(request.body)
 }
+
+// export async function POST(){
+//     return NextResponse.json('Get request');
+// }
